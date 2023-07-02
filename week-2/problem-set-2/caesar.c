216e6d3756c25db0c36d2dfa3ed64323a3f3d604
Hiccup-Dusk <shuaib.abdurahman22@gmail.com>
2023-07-02 23:44:14 +0800

    update code

--- week-2/problem-set-2/caesar.c
+++ week-2/problem-set-2/caesar.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 #include <cs50.h>
-
 #include <string.h>
-
 #include <stdlib.h>
-
 #include <ctype.h>
+
+int shiftChar(char c, int k);
 
 int main(int argc, string argv[])
 {
@@ -15,39 +13,42 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    int k = atoi(argv[1]);
-    if (k < 0)
+    
+    // Check if the key is a valid non-negative integer
+    string key = argv[1];
+    for (int i = 0; i < strlen(key); i++)
     {
-        printf("Usage: ./caesar key\n");
-        return 2;
+        if (!isdigit(key[i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
     }
+    
+    int k = atoi(key);
 
     string text = get_string("Text: ");
-
     printf("ciphertext: ");
 
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if (isalpha(text[i]))
-        {
-            if (isupper(text[i]))
-            {
-                char cipher_num_capital = ((text[i] - 65 + k) % 26) + 65;
-                printf("%c", cipher_num_capital);
-            }
-
-            if (islower(text[i]))
-            {
-                char cipher_num_small = ((text[i] - 97 + k) % 26) + 97;
-                printf("%c", cipher_num_small);
-            }
-        }
-        else
-        {
-            printf("%c", text[i]);
-        }
+        char encryptedChar = shiftChar(text[i], k);
+        printf("%c", encryptedChar);
     }
 
     printf("\n");
     return 0;
+}
+
+int shiftChar(char c, int k)
+{
+    if (isalpha(c))
+    {
+        int base = isupper(c) ? 'A' : 'a';
+        return ((c - base + k) % 26) + base;
+    }
+    else
+    {
+        return c;
+    }
 }
